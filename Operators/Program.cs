e6d69c16fa5f0092143099d57d85271ecfffd7d7
Leigh Hobson <leighhobson89@outlook.com>
2020-04-10 16:08:22 +0200

    WholeCourse

---
+++ Operators/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10; //define two variables with '=' operator and use '+' to add them
+            var b = 3;
+
+            Console.WriteLine(a + b);
+
+            var c = 10; //define two variables with '=' operator and use '/' to divide them (use float casting to get more precise decimal result otherwise integer is written)
+            var d = 3;
+
+            Console.WriteLine((float)c / (float)d);
+
+            var e = 1; //operator precedence (BODMAS - Division and Multiplication have higher precedence than Addition and Subtraction, result = 7)
+            var f = 2;
+            var g = 3;
+
+            Console.WriteLine(e + f * g);
+
+            var h = 1; //operator precedence (Parentheses give higher precedence to values inside them, result = 9)
+            var i = 2;
+            var j = 3;
+
+            Console.WriteLine((h + i) * j);
+
+            var k = 1; //comparison operators return boolean values
+            var l = 2;
+
+            Console.WriteLine(k > l); //false
+            Console.WriteLine(k < l); //true
+            Console.WriteLine(k == l); //false
+            Console.WriteLine(k != l); //true
+            Console.WriteLine(!(k != l)); //False (because two negatives is a positive, bad practice, better to use one positive (k == l)
+
+            var m = 1; //logical operators - return as true if both, or one or the other, or none are true, otherwise false
+            var n = 2;
+            var o = 3;
+
+            Console.WriteLine(o > n && o > m); //true
+            Console.WriteLine(o > n && o == m); //false (only one condition returned true and the AND operator was used)
+            Console.WriteLine(o > n || o > m); //true (both true is true)
+            Console.WriteLine(o > n || o == m); //true (only OR the other true is true)
+            Console.WriteLine(o < n || o == m); //false (none are true is false)
+            Console.WriteLine(!(o > n || o == m)); //NOT reverses the outcome of the comparisons in the parentheses
+        }
+    }
+}
---
+++ TypeConversion/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //implicit conversion
+            byte a = 1;
+            int i = a;
+            Console.WriteLine(i);
+
+            //explicit conversion(casting)
+            int b = 2; // if value higher than 255 is used, data loss occurs as value is higher than max range of 'byte' data type, so value is wrong (e.g. 2000 gives 208)
+            byte j = (byte)b;
+            Console.WriteLine(j);
+
+            //non compatible conversions - string => integer
+            string number = "1234";
+            int k = Convert.ToInt32(number);
+            Console.WriteLine(k);
+
+            //non compatible conversions - string => boolean
+            string str = "true";
+            bool l = Convert.ToBoolean(str);
+            Console.WriteLine(l);
+
+            //non compatible conversions to lesser data types (causes exception "Value was either too large or too small for an unsigned byte")
+            try //try stops crashing by running catch block if this following code fails
+            {
+                string numberThree = "1234";
+                int m = Convert.ToByte(numberThree);
+                Console.WriteLine(m);
+            }
+            catch (Exception) // outputs a friendly message to user and stops crashing.
+            {
+                Console.WriteLine("The number could not be converted to a byte.");
+            }
+        }
+    }
+}
