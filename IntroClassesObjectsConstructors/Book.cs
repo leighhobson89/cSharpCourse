47c5aa6afc207d2b0be70443af3401ee0cebfc0d
Leigh Hobson <leighhobson89@outlook.com>
2020-04-11 22:29:56 +0200

    ClassesObjectsConstructors

---
+++ IntroClassesObjectsConstructors/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntroClassesObjectsConstructors
+{
+    class Book //think of a class as a custom data type blueprint. It contains "class attributes" which are variables declared inside the class
+    {
+        public string title; //class attributes
+        public string author; 
+        public int pages;
+
+        public Book(string aTitle, string aAuthor, int aPages) //constructor named as "public <nameOfClass>()", takes optional parameters from main program when it defines objects of this class and executes code upon creation of object
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+
+            Console.WriteLine("Created " + title + " with " + pages + " pages, by " + author + "."); //example output summarises the data passed in to the class constructor when defining the object in the main program
+        }
+    }
+}
---
+++ IntroClassesObjectsConstructors/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IntroClassesObjectsConstructors
+{
+    class Program
+    {
+        static void Main(string[] args) //An object is an instance of a class i.e. a physical object made up from the class template or "blueprint" referred to when creating it
+        {
+            Book book1 = new Book("The Return of Harry Potter","J.K. Rowling",400); //creation of book object "Book", then "book1" is the name given to the object, and then "new Book()" is saying we will make an instance of the Book template and pass in its information in the parentheses as defined in the class template constructor, and then it will be set up
+            Book book2 = new Book("The Good Old Lord Of The Rings","J.R.R. Tolkien", 720);
+
+            //Once they are setup they can be used in the main program and the coder can refer to any attributes or methods of the object that it contains ie book1.author or book2.pages etc and it will use the values passed in for those particular attributes
+            int howManyPagesDoesBook2Have = book2.pages; // example of pulling out the data and using it to write to the console the number of pages for the book2 object
+            Console.WriteLine(howManyPagesDoesBook2Have); //720 is output
+            book2.pages = 1000; // can redefine an objects attribute
+            int howManyPagesDoesBook2HaveNow = book2.pages;
+            Console.WriteLine(howManyPagesDoesBook2HaveNow); //1000 is output
+        }
+    }
+}
