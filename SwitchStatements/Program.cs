cbb8619fff0f2806a16c0bab2ae1605a132cdc13
Leigh Hobson <leighhobson89@outlook.com>
2020-04-11 16:35:58 +0200

    switch and while do while loops added

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Convert numbers to days of the week
+            Console.WriteLine(GetDay(1)); //pass in a day of the week in number format and write result as a day
+        }
+
+        static string GetDay(int dayNum)
+        {
+            string dayName;
+
+            switch(dayNum)
+            {
+                case 0: //if number passed in is 0, then set dayName to be "Sunday"
+                    dayName = "Sunday";
+                    break; //required if want to make it stop checking after first one is true
+                case 1:
+                    dayName = "Monday";
+                    break;
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+                case 3:
+                    dayName = "Wednesday";
+                    break;
+                case 4:
+                    dayName = "Thursday";
+                    break;
+                case 5:
+                    dayName = "Friday";
+                    break;
+                case 6:
+                    dayName = "Saturday";
+                    break;
+                default: //if none are true then return this value
+                    dayName = "Invalid Day Number";
+                    break;
+            }
+
+            return dayName;
+        }
+    }
+}
---
+++ While Loop/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace While_DoWhile_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //while loop checks condition, then runs code
+        //do_while loop runs code, then checks condition so will always run at least once.
+
+        {
+            int index = 1;
+            while (index <= 2) //while loops can be infinite if condition never becomes false so will cause a crash
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+
+            int indexTwo = 6;
+            do
+            {
+                Console.WriteLine(indexTwo);
+                indexTwo++;
+            }
+            while (indexTwo <= 3);
+        }
+    }
+}
