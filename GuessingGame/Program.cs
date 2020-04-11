0623dc035e32cdf95c5371930adb220ffb285ede
Leigh Hobson <leighhobson89@outlook.com>
2020-04-11 17:18:45 +0200

    guessing game index updated

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -8,19 +8,19 @@ namespace GuessingGame
         {
             string secretWord = "leigh"; //set up secret word
             string guess = ""; //initialise guess variable
-            int index = 1;
+            int index = 0;
 
             do
             {
-                Console.Write("Guess " + index + ": What's the secret word?"); //ask for guess
+                Console.Write("Guess {0}: What's the secret word?", index+1); //ask for guess (add one because index initialised at 0)
                 guess = Console.ReadLine(); //read it in to guess variable
                 index++;
             }
-            while (secretWord != guess && index < 6); //check if words match, if no loop again, if they do then break loop
+            while (secretWord != guess && index < 5); //check if words match, if no loop again, if they do then break loop
 
-            if (index <= 6 && guess == secretWord) //once loop broken, check if guessed correctly within 5 guesses (use 6 as index variable initialised at 1)
+            if (index <= 5 && guess == secretWord) //once loop broken, check if guessed correctly within 5 guesses
             {
-                Console.WriteLine("Congratulations, you guessed it right on guess {0}!", index-1); //success message tells user which guess they got it on, minus one because variable initialised at 1
+                Console.WriteLine("Congratulations, you guessed it right on guess {0}!", index); //success message tells user which guess they got it on
             }
             else
             {
