86e043f7b4b20643678d7542098ec75c08bd5594
Leigh Hobson <leighhobson89@outlook.com>
2020-04-11 15:25:33 +0200

    if statements using comparisons added

---
+++ Comparisons/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Comparisons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetMax(2, 10)); //pass two different integers and write out largest one
+            Console.WriteLine(GetMaxy(18, 16, 15)); //pass three different integers and write out largest one
+        }
+
+
+        static int GetMax(int num1, int num2) //compare 2
+        {
+            int result;
+
+            if (num1 > num2) //check which is bigger
+            {
+                result = num1; //if num1 > num2 set return variable to num1 
+            }
+            else
+            {
+                result = num2; // else set it to num2
+            }
+
+            return result; //return which is bigger
+        }
+        static int GetMaxy(int num1, int num2, int num3) //compare 3
+        {
+            int result;
+
+            if (num1 > num2 && num1 > num3) //check if num1 is biggest
+            {
+                result = num1; // and set num1 as return variable
+            }
+            else if (num2 > num1 && num2 > num3) //check if num2 is biggest
+            {
+                result = num2; // and set num2 as return variable
+            }
+            else
+            {
+                result = num3; // otherwise set num3 as return variable
+            }
+
+            return result; //return which is bigger
+        }
+    }
+}
---
+++ IfStatements/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace IfStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            I wake up                
+            if I’m hungry                    ----condition true/false
+                I eat breakfast              ----action if true
+            
+            I look at my phone
+            if it’s about to die             ----condition true/false
+                I charge it                  ----action if true
+
+            I leave my house
+            if it’s cloudy                   ----condition true/false
+                I bring an umbrella          ----action if true
+            otherwise
+                bring sunglasses             ----action if not true
+            */
+            bool isMale = true;
+            bool isTall = false;
+
+            if (isMale) // simple way to check if a boolean variable is set to true
+            {
+                Console.WriteLine("You are male");
+            } else
+            {
+                Console.WriteLine("You are not male");
+            }
+
+            if (isMale && isTall) // using AND in condition
+            {
+                Console.WriteLine("You are a tall male");
+            } 
+            else
+            {
+                Console.WriteLine("You are either not male or not tall or both");
+            }
+
+            if (isMale || isTall) // using OR in condition
+            {
+                Console.WriteLine("You are either tall or male or both");
+            }
+            else
+            {
+                Console.WriteLine("You are neither male nor tall");
+            }
+
+            if (isMale && isTall) // using AND in condition
+            {
+                Console.WriteLine("You are a tall male");
+            }
+            else if (isMale && !isTall) //if male but NOT tall (negation operator)
+            {
+                Console.WriteLine("You are a short male");
+            }
+            else if (!isMale && isTall) //if NOT male but tall (negation operator)
+            {
+                Console.WriteLine("You are not a male but you are tall");
+            }
+            else
+            {
+                Console.WriteLine("You are not male and not tall");
+            }
+        }
+    }
+}
